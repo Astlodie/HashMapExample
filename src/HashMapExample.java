e3a1d9a463021f40fe3dad80576ffd296e20987a
ballaaddict40 <ballaaddict40@localhost>
2021-03-25 13:48:58 -0400

    2nd commit. I added input=0 and I removed input=10.

--- src/HashMapExample.java
+++ src/HashMapExample.java
@@ -5,36 +5,34 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class HashMapExample {
-	
+
 	public static void main(String args[]) {
 
-	      /* This is how to declare HashMap */
-	      HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+		/* This is how to declare HashMap */
+		HashMap<Integer, String> hmap = new HashMap<Integer, String>();
 
-	      /*Adding elements to HashMap*/
-	      hmap.put(1, "One");
-	      hmap.put(2, "Two");
-	      hmap.put(3, "Three");
-	      hmap.put(4, "Four");
-	      hmap.put(5, "Five");
-	      hmap.put(6, "Six");
-	      hmap.put(7, "Seven");
-	      hmap.put(8, "Eight");
-	      hmap.put(9, "Nine");
-	      hmap.put(10, "Ten");
+		/*Adding elements to HashMap*/
+		hmap.put(0, "Zero");
+		hmap.put(1, "One");
+		hmap.put(2, "Two");
+		hmap.put(3, "Three");
+		hmap.put(4, "Four");
+		hmap.put(5, "Five");
+		hmap.put(6, "Six");
+		hmap.put(7, "Seven");
+		hmap.put(8, "Eight");
+		hmap.put(9, "Nine");
 
-	      /* Display content using Iterator*/
-	      Set<Entry<Integer, String>> set = hmap.entrySet();
-	      Iterator<Entry<Integer, String>> iterator = set.iterator();
-	      while(iterator.hasNext()) {
-	         @SuppressWarnings("rawtypes")
+
+		/* Display content using Iterator*/
+		Set<Entry<Integer, String>> set = hmap.entrySet();
+		Iterator<Entry<Integer, String>> iterator = set.iterator();
+		while(iterator.hasNext()) {
+			@SuppressWarnings("rawtypes")
 			Map.Entry mentry = (Map.Entry)iterator.next();
-	         System.out.print("The input is: "+ mentry.getKey() + " & The value is: ");
-	         System.out.println(mentry.getValue());
-	      }
+			System.out.print("The input is: "+ mentry.getKey() + " & The value is: ");
+			System.out.println(mentry.getValue());
 
-	      /* Get values based on key*/
-	      String var= hmap.get(2);
-	      System.out.println("The value at index 2 is: "+var);
-}
+		}
+	}
 }
